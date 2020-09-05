79b137a12c2788ee205eac7d1f65446b29052eb6
Fabio <fabio.sangregorio@gmail.com>
2020-09-05 09:52:31 +0200

    Add build files from private repo

---
+++ Gruntfile.js
@@ -0,0 +1,247 @@
+const models = require("./src/db/models.js");
+const sass = require("node-sass");
+const imagemin = {
+  mozjpeg: require('imagemin-mozjpeg'),
+  gifsicle: require('imagemin-gifsicle'),
+  jpegtran: require('imagemin-jpegtran'),
+  optipng: require('imagemin-optipng'),
+  svgo: require('imagemin-svgo')
+}
+
+module.exports = function (grunt) {
+  // Path relative to js folder
+  var vendorScripts = [
+    "/vendor/flickity.pkgd.min.js",
+    "/vendor/flickity-fade.js",
+    "/vendor/aos.js"
+  ];
+  // Path relative to js folder
+  var appScripts = [
+    "/util.js",
+    "/dark-theme.js",
+    "/app.js"
+  ]
+
+  // Path relative to css folder
+  var css = [
+    '/aos.css',
+    '/flickity.min.css',
+    '/flickity-fade.css',
+    '/googlefonts.css',
+  ];
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: { implementation: sass },
+        files: { 'dist/css/app.css': 'src/sass/app.sass' }
+      }
+    },
+
+    cssmin: {
+      dist: { 
+        files: { 
+          'dist/css/bundle.min.css': [
+            css.map(f => 'src/css' + f),
+            'dist/css/app.css'
+          ] 
+        } 
+      }
+    },
+
+    ejs: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/views',
+          src: ['**/*.ejs', '!**/_*.ejs', '!**/partials/**', '!**/components/**', '!**/projects/**'],
+          dest: 'dist/',
+          ext: '.html'
+        }],
+        options: models
+      }
+    },
+
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ['es2015'],
+        plugins: ['transform-remove-strict-mode']
+      },
+      src: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['js/**/*.js', '!js/vendor/**/*.js'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          beautify: true,
+          mangle : false, 
+          compress: false
+        },
+        files: { 
+          'dist/js/bundle.min.js': [
+            vendorScripts.map(f => 'src/js' + f),
+            appScripts.map(f => 'dist/js' + f),
+          ]
+        }
+      },
+      dist: {
+        options: {
+          beautify: false,
+          mangle : true, 
+          compress: true
+        },
+        files: { 
+          'dist/js/bundle.min.js': [
+            vendorScripts.map(f => 'src/js' + f),
+            appScripts.map(f => 'dist/js' + f),
+          ]
+        }
+      }
+    },
+
+    imagemin: {
+      dist: {
+        options: { 
+          optimizationLevel: 1,
+          use: [
+            imagemin.mozjpeg({ quality: 90 }), 
+            imagemin.gifsicle(), 
+            imagemin.jpegtran({ progressive: true }), 
+            imagemin.optipng(), 
+            imagemin.svgo()
+          ]
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**/*.{png,jpg,gif}', '!**/projects/**/*'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    cwebp: {
+      dist: {
+        options: { q: 100 },
+        files: [{
+          expand: true,
+          cwd: 'dist/images/', 
+          src: ['**/*.{png,jpg}'],
+          dest: 'dist/images/webp/'
+        }]
+      }
+    },
+
+    sync: {
+      dist: {
+        files: [{
+          cwd: 'src/',
+          src: [
+            '**',
+            '!**/*.{png,jpg,gif}',
+            '!**/sass/**',
+            '!**/components/**',
+            '!**/partials/**',
+            '!**/*.sass',
+            '!**/*.ejs',
+            '!**/js/**',
+            '!**/css/**',
+            '!**/db/**'
+          ],
+          dest: 'dist/'
+        }],
+        verbose: true
+      }
+    },
+
+    clean: {
+      dist: {
+        src: [
+          'dist/views/**',
+          'dist/css/**/*',
+          '!dist/css/bundle.min.css',
+          'dist/js/**/*',
+          '!dist/js/bundle.min.js',
+          'dist/images/projects/**'
+        ]
+      },
+      reset: { src: ['dist/'] }
+    },
+
+    sitemap: {
+      dist: {
+        pattern: ['dist/**/*.html', '!**/google*.html'], // this will exclude 'google*.html'
+        siteRoot: 'dist/',
+        homepage: 'https://fabio.sangregorio.dev/'
+      }
+    },
+
+    browserSync: {
+      dist: {
+        bsFiles: { src: ['dist/**/*.*'] },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: './dist',
+            index: 'index.html'
+          }
+        }
+      }
+    },
+    
+    watch: {
+      js: {
+        files: ['src/**/*.js'],
+        tasks: ['babel', 'sync', 'uglify:dev', 'clean:dist']
+      },
+      html: {
+        files: ['src/**/*.ejs'],
+        tasks: ['ejs']
+      },
+      sass: {
+        files: ['src/**/*.sass'],
+        tasks: ['sass', 'sync', 'cssmin', 'clean:dist']
+      },
+      images: {
+        files: ['src/**/*.{png,jpg,gif}'],
+        tasks: ['imagemin', 'cwebp', 'clean:dist']
+      },
+      default: {
+        files: [
+          'src/**/*.*',
+          '!src/**/*.js',
+          '!src/**/*.ejs',
+          '!src/**/*.sass',
+          '!src/**/*.{png,jpg,gif}'
+        ],
+        tasks: ['sync', 'clean:dist']
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-ejs');
+  grunt.loadNpmTasks('grunt-ejs-locals');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-sitemap');
+  grunt.loadNpmTasks('grunt-cwebp');
+
+  grunt.registerTask('default', ['clean:reset', 'sass', 'babel', 'ejs', 'sync', 'cssmin', 'uglify:dev', 'imagemin', 'cwebp', 'clean:dist',  'browserSync', 'watch']);
+  grunt.registerTask('build', ['clean:reset', 'sass', 'babel', 'ejs', 'sync', 'cssmin', 'uglify:dist', 'imagemin', 'cwebp', 'clean:dist', 'sitemap']);
+};
---
+++ src/db/models.js
@@ -0,0 +1,167 @@
+module.exports = {
+  projects: [
+    {
+      id:            'noku',
+      title:         'Noku TokenRaise',
+      description:   'Noku TokenRaise is a web app that provides you with the best service on the market to customize your crowdsale on Ethereum Blockchain, without writing a line of code and in a matter of minutes.',
+      category:      'React web app',
+      year:          '2018',
+      link: {
+        text:        'tokenraise.noku.io',
+        url:         'https://www.noku.io/tokenraise/'
+      },
+      desktopImages: '4',
+      mobileImages:  '3',
+      role:          'to do'
+    }, {
+      id:            'netmedicare',
+      title:         'Net Medicare',
+      description:   'to do',
+      category:      'HTML/jQuery web app',
+      year:          '2017',
+      link: {
+        text:        'netmedi.care',
+        url:         'https://www.netmedi.care/'
+      },
+      desktopImages: '4',
+      mobileImages:  '3',
+      role:          'to do'
+    }, {
+      id:            'parlux',
+      title:         'MyParlux',
+      description:   'to do',
+      category:      'HTML/jQuery website',
+      year:          '2016',
+      role:          'to do',
+      desktopImages: '4',
+      mobileImages:  '3',
+    }
+  ],
+  jobs: [
+    {
+      title:         'Yourbiz',
+      link:          'https://www.yourbiz.it',
+      role:          'Frontend web developer',
+      start:         'July 2016',
+      end:           'Current',
+      description:   "I've been working at Yourbiz as a part-time developer for 3 years, where I've been building the frontend of the company websites, with special attention to responsiveness, SEO and performance optimization. I've also had experience in the realization of pure HTML/JS web apps, as well as apps based on frameworks such as Angular, AngularJS and React."
+    }, {
+      title:         'Sorint.LAB',
+      link:          'https://www.sorint.it',
+      role:          'R&D dpt. school internship',
+      start:         'June 2015',
+      end:           'June 2015',
+      description:   "During the school internship at Sorint I studied the OrientDB graph database, and I built a Java web app for internal use with the Spring framework and Hibernate. I also attended the following corporate training courses: VMware, OrientDB, MySQL, Java, Spring Framework 4.0, Hibernate, Spring Security."
+    }
+  ],
+  education: [
+    {
+      title:         'Bergamo University',
+      link:          'https://www.unibg.it/',
+      role:          'Computer engineering bachelor’s degree ',
+      start:         'Sept. 2016',
+      end:           'Current',
+      description:   "I'm currently attending the first year of my Master's degree in Computer Engineering, where I completed the following core courses: Information Technology 2, Databases 2, Industrial Automation, Software Engineering, Statistics 2, Management Control Systems."
+    }, {
+      title:         'IIS G. Marconi',
+      link:          'https://itisdalmine.edu.it/',
+      role:          'IT high school diploma',
+      start:         'June 2016',
+      end:           'Sept. 2011',
+      description:   "Core courses: C#, MVC, web API, Database, Client-Server architecture, HTML/CSS, Javascript, jQuery.<br />Activities: CCNA1 course, \"Impresa in Azione\" project.<br />Thesis: assigning internships to students based on skills levels."
+    }
+  ],
+  skills: [
+    [
+      ['HTML5 & CSS3', 'Javascript', 'Python', 'Sass'],
+      ['jQuery', 'React', 'Git'],
+      ['Github']
+    ], [
+      ['Ruby (on Rails)', 'Typescript', 'MATLAB', 'C#'],
+      ['Angular', 'Grunt', 'Node'],
+      ['Photoshop']
+    ], [
+      ['Java', 'C++', 'SQL', 'PHP'],
+      ['AngularJS', 'Firebase', 'npm'],
+      ['Figma']
+    ]
+  ],
+  coding: [
+    {
+      title: 'telereddit',
+      description: 'A reddit bot for Telegram written in python. When the user sends a message containing the name of a subreddit (e.g. “r/funny”), the bot will send a random post from that subreddit. When a link from reddit is shared in the chat, the bot will retrieve the post and send it as a message.',
+      link: {
+        url: 'https://github.com/fabiosangregorio/telereddit',
+        title: 'github/telereddit'
+      }
+    }, {
+      title: 'remindme',
+      description: 'A reminder bot for Telegram.<br/>You can reply to a Telegram message with “!remindme 2h 5m” and the bot will remind you that message (with a link to it) in two hours and five minutes. As you might have guessed, I really like reddit and Telegram :)',
+      link: {
+        url: 'https://github.com/fabiosangregorio/remindme',
+        title: 'github/remindme'
+      }
+    }, {
+      title: 'links-disabler',
+      description: 'I often find myself making some changes in the Chrome Dev Tools while developing, and sometimes I would accidentally click a link, losing them. This is a really simple Chrome extension which you can use to quickly disable all the links in a web page.',
+      link: {
+        url: 'https://github.com/fabiosangregorio/links-disabler',
+        title: 'github/links-disabler'
+      }
+    }
+  ],
+  photography: [
+    {
+      image: {
+        path: '/images/church.jpg',
+        alt: 'Basilica of Santa Maria Maggiore, Bergamo',
+        description: 'Basilica of Santa Maria Maggiore, Bergamo'
+      },
+      link: {
+        url: 'https://instagram.com/fabio.sang',
+        title: 'instagram/fabio.sang'
+      }
+    }, {
+      image: {
+        path: '/images/orlandini.jpg',
+        alt: 'An artist with his artwork',
+        description: 'An artist with his artwork'
+      },
+      link: {
+        url: 'https://photography.fabiosangregorio.tk',
+        title: 'fabiosangregorio/photography'
+      }
+    }, {
+      image: {
+        path: '/images/guitar2.jpg',
+        alt: 'A custom guitar @ Music Wall, Pizzighettone',
+        description: 'A custom guitar @ Music Wall, Pizzighettone'
+      },
+      link: {
+        url: 'https://unsplash.com/fabiosangregorio',
+        title: 'unsplash/fabiosangregorio'
+      }
+    }
+  ],
+  drawing: [
+    {
+      image: {
+        path: '/images/hands.jpg',
+        alt: 'Two hands joined together.',
+        description: 'Two hands joined together.'
+      },
+    }, {
+      image: {
+        path: '/images/skull.jpg',
+        alt: 'A rose growing from a dead skull.',
+        description: 'A rose growing from a dead skull.'
+      },
+    }, {
+      image: {
+        path: '/images/head.jpg',
+        alt: 'Half skin half skull.',
+        description: 'Half skin half skull.'
+      },
+    }
+  ]
+}
